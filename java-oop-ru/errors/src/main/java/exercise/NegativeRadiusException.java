cdcaeaf6cb7c491f74fc1091e24224b27011953f
@hexlet/cli <support@hexlet.io>
2022-11-15 19:48:33 -0500

    submit java-oop-ru/errors

---
+++ java-oop-ru/errors/src/main/java/exercise/NegativeRadiusException.java
@@ -0,0 +1,14 @@
+package exercise;
+
+// BEGIN
+class NegativeRadiusException extends Exception {
+    public NegativeRadiusException() {
+        super();
+    }
+
+    public NegativeRadiusException(String message) {
+        super(message);
+    }
+}
+
+// END
